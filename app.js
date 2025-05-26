1ab3e3af509bb215bd6fc8ab8ad9cc78739c6822
Shoaib khan <shoaibmangal@yahoo.com>
2025-05-27 00:27:56 +0500

    add files

---
+++ app.js
@@ -0,0 +1,85 @@
+fetch("https://api.thedogapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1")
+.then((data) => {
+  console.log(data)
+})
+.catch((error) => {
+  console.log(error)
+})
+
+
+new Promise (function(resolve, reject){
+  setTimeout(function(){
+    console.log("promise resolved")
+    resolve()
+  }, 3000)
+})
+
+
+const promiseTwo = new Promise(function(resolve,reject){
+  setTimeout(function(){
+     resolve({userName: "shoaib", email: "abc.example@gmail.com"})
+  },2000)
+});
+
+promiseTwo.then(function(user){
+   console.log(user)
+})
+
+const promiseThree = new Promise(function(resolve,reject){
+  setTimeout(function(){
+    let error = false;
+    if(!error){
+      resolve({name: "shoaib", password: "12345"})
+    } else {
+      reject("something went wrong")
+    }
+  } ,3000)
+});
+
+promiseThree.then((user) =>{
+ console.log(user)
+ return user.userName
+})
+.then((userName) =>{
+  console.log(userName)
+})
+.catch((error) =>{
+  console.log(error)
+})
+.finally(() =>{
+  console.log("the promise is either resolved or rejected")
+})
+
+
+const promiseFive = new Promise(function(resolve, reject){
+  setTimeout(function(){
+    let error = true
+    if(!error){
+      resolve({name: "javascript", password: "12345"})
+    }else{
+      reject("something went wrong")
+    }
+
+  } ,4000)
+});
+
+promiseFive.then(() =>{
+  console.log(user)
+  return user.userName
+})
+.then(function(userName){
+  console.log(userName)
+})
+.catch((error) =>{
+  console.log(error)
+})
+.finally(() =>{
+  console.log("the promise is either resolve or rejected")
+})
+
+async function consumePromiseFive(){
+  const response = await promiseFive
+  console.log(response)
+}
+
+consumePromiseFive()
